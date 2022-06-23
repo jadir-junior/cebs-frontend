67ef0a025d53a3991a1542a820bb9700e8975d03
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-06-23 16:02:10 -0300

    add title, text, input name, email, password

--- src/app/modules/authentication/register/register.component.ts
+++ src/app/modules/authentication/register/register.component.ts
@@ -1,20 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+
+import { Component } from '@angular/core'
+import { ValidationService } from 'src/app/common/validation.service'
 
 @Component({
   selector: 'app-register',
-  template: `
-    <p>
-      register works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: 'register.component.html',
+  styleUrls: ['register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  showPassword = false
+  submitted = false
 
-  constructor() { }
+  form: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      Validators.compose([
+        Validators.required,
+        this.validation.passwordPatternValidator(),
+      ]),
+    ],
+  })
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private validation: ValidationService) {}
+
+  get name(): FormControl {
+    return this.form.get('name') as FormControl
   }
 
+  get email(): FormControl {
+    return this.form.get('email') as FormControl
+  }
+
+  get password(): FormControl {
+    return this.form.get('password') as FormControl
+  }
+
+  toggleVisibility(): void {
+    this.showPassword = !this.showPassword
+  }
+
+  onSubmit({ value, valid }: FormGroup): void {
+    this.submitted = true
+    console.log('FORM VALUE', value)
+    if (valid) {
+      const { name, email, password } = value
+    }
+  }
 }
