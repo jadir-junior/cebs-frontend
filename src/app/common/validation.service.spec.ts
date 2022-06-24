1d53823d30641d94aa416738997e6aea33ad7aba
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-06-24 14:10:52 -0300

    config spectator

--- src/app/common/validation.service.spec.ts
+++ src/app/common/validation.service.spec.ts
@@ -1,16 +1,15 @@
-import { TestBed } from '@angular/core/testing';
-
-import { ValidationService } from './validation.service';
+import { TestBed } from '@angular/core/testing'
+import { ValidationService } from './validation.service'
 
 describe('ValidationService', () => {
-  let service: ValidationService;
+  let service: ValidationService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ValidationService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(ValidationService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
