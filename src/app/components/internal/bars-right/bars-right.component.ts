902336a25841abbf5baf74f09a1e24496a1bae34
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-07-04 21:35:11 -0300

    :construction: add bars

--- src/app/components/internal/bars-right/bars-right.component.ts
+++ src/app/components/internal/bars-right/bars-right.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'cebs-bars-right',
   template: `
-    <div class="wrapper-bars-right">
+    <div class="wrapper-bars-right" aria-label="bars-right">
       <cebs-icon-button icon="search"></cebs-icon-button>
       <cebs-icon-button icon="bar_chart"></cebs-icon-button>
       <cebs-icon-button icon="notifications"></cebs-icon-button>
